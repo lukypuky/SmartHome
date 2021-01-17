b33ea4bf430cadb49770d80fcf833223f7fc39a8
lukypuky <lukash.baca@gmail.com>
2021-01-17 18:12:20 +0100

    pridany dialog screen pre pridavanie miestnosti

--- app/src/main/java/com/example/smarthome/main/AddRoom_popup.java
+++ app/src/main/java/com/example/smarthome/main/AddRoom_popup.java
@@ -3,16 +3,19 @@ package com.example.smarthome.main;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.smarthome.R;
 
-public class AddRoom_screen extends AppCompatActivity
+public class AddRoom_popup extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_room_screen);
+        setContentView(R.layout.activity_add_room_popup);
     }
 }
--- app/src/main/java/com/example/smarthome/main/Main_screen.java
+++ app/src/main/java/com/example/smarthome/main/Main_screen.java
@@ -2,15 +2,22 @@ package com.example.smarthome.main;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -26,16 +33,24 @@ import java.util.ArrayList;
 
 public class Main_screen extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
 {
+    //menu
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
     private Toolbar toolbar;
 
+    //pridanie miestnosti
     private Button addRoom;
+    private AlertDialog.Builder addRoomDialog;
+    private AlertDialog dialog;
+    private EditText roomName;
+    private Spinner roomType;
+    private Button saveRoom;
+    private Button unsaveRoom;
 
+    //miestnosti
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter; // bridge medzi datami a recycler view
     private RecyclerView.LayoutManager mLayoutManager;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -45,19 +60,21 @@ public class Main_screen extends AppCompatActivity implements NavigationView.OnN
 
         //tlacidlo na pridanie novej miestnosti
         addRoom = (Button) findViewById(R.id.addRoom);
-
         addRoom.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                openNewRoomActivity();
+                addRoomDialog();
             }
         });
 
         //miesnosti ktore si vie user pridat
         ArrayList<RoomItem> roomList = new ArrayList<>();
         roomList.add(new RoomItem(R.drawable.kitchen,"Kuchyňa","blabla"));
+        roomList.add(new RoomItem(R.drawable.kitchen,"Kuchyňa","baaaaaaaaaa"));
+        roomList.add(new RoomItem(R.drawable.kitchen,"Kuchyňa","ggggggggggg"));
+        roomList.add(new RoomItem(R.drawable.kitchen,"Kuchyňa","base qwfqwfasla"));
         roomList.add(new RoomItem(R.drawable.livingroom,"Obývačka","asdasdas"));
 
         //plocha pre miestnosti v domacnosti
@@ -84,13 +101,6 @@ public class Main_screen extends AppCompatActivity implements NavigationView.OnN
 
         //pri spusteni appky bude zakliknuta defaultne main screena
         navigationView.setCheckedItem(R.id.mainScreen);
-    }
-
-    //metoda na pridanie novej miestnosti do domacnosti a na main screen
-    public void openNewRoomActivity()
-    {
-        Intent addRoom_intent = new Intent(Main_screen.this, AddRoom_screen.class);
-        startActivity(addRoom_intent);
     }
 
     //pri pouziti tlacidla "Back" alebo po pouziti gesta na vratenie spat, sa zasunie menu, miesto toho aby sa vratilo a obrazovku spat
@@ -132,5 +142,60 @@ public class Main_screen extends AppCompatActivity implements NavigationView.OnN
 
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    //metoda na pridanie novej miestnosti v domacnosti
+    public void addRoomDialog()
+    {
+        addRoomDialog = new AlertDialog.Builder(Main_screen.this);
+        View contactPopupView = getLayoutInflater().inflate(R.layout.activity_add_room_popup, null);
+
+        roomName = (EditText) contactPopupView.findViewById(R.id.roomName);
+        saveRoom = (Button) contactPopupView.findViewById(R.id.saveRoomButton);
+        unsaveRoom = (Button) contactPopupView.findViewById(R.id.unsaveRoomButton);
+
+        roomType = (Spinner) contactPopupView.findViewById(R.id.roomType);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(Main_screen.this,
+                android.R.layout.simple_spinner_item,
+                getResources().getStringArray(R.array.rooms));
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        roomType.setAdapter(adapter);
+
+        addRoomDialog.setView(contactPopupView);
+        dialog = addRoomDialog.create();
+        dialog.show();
+
+        saveRoom.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (roomName.getText().toString().isEmpty())
+                {
+                    Toast.makeText(Main_screen.this, "Zadajte názov pre miestnosť", Toast.LENGTH_SHORT).show();
+                }
+
+                else if(roomType.getSelectedItem().toString().equals("Typ miestnosti"))
+                {
+                    Toast.makeText(Main_screen.this, "Zvoľte typ miestnosti", Toast.LENGTH_SHORT).show();
+                    //vypis message nech sa zvoli miestnost
+                }
+
+                else
+                {
+                    Toast.makeText(Main_screen.this, "Miestnosť pridaná", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        unsaveRoom.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                dialog.dismiss();
+            }
+        });
     }
 }
